ef10627ece1f816a13c93d27bcb5f3d72dc5bac4
CANOST-id <csinner31@gmail.com>
2025-11-17 00:50:44 +0100

    added arrays and functions to draw() to display elements

--- models/world.class.js
+++ models/world.class.js
@@ -1,9 +1,48 @@
 class World {
     character = new Character();
     enemies = [
+        new Chicken(),
+        new Chicken(),
         new Chicken()
     ];
+    clouds = [
+        new Cloud(),
+        new Cloud(),
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/air.png'),
+        new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/3_third_layer/1.png'),
+        new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/2_second_layer/1.png'),
+        new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/1_first_layer/1.png')
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+
+        requestAnimationFrame(() => this.draw());
     }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    };
 }
