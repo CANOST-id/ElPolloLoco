99012b890ebb784468e7eb08fcf7cbb8f5a41644
CANOST-id <csinner31@gmail.com>
2025-11-17 00:47:49 +0100

    added function for chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,3 +1,11 @@
 class Chicken extends MovableObject {
 
+    constructor() {
+        super().loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
+
+        this.y = 340;
+        this.x = 200 + Math.random() * 500;
+        this.height = 80;
+        this.width = 80;
+    }
 }
