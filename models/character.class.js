050f9fa34f60082f3ff6e5f2ab25989d266d476a
CANOST-id <csinner31@gmail.com>
2025-11-17 00:48:27 +0100

    edited positioning and size of character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,11 @@ class Character extends MovableObject {
 
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+
+        this.height = 250;
+        this.width = 100;
+        this.y = 175;
+        this.x = 100;
     }
 
     jump() {
