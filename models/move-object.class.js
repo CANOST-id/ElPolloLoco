3d78f478b24c225e7e1888e279a24a12392cc111
CANOST-id <csinner31@gmail.com>
2025-11-17 21:11:43 +0100

    added main function for animation/animated elements

--- models/move-object.class.js
+++ models/move-object.class.js
@@ -3,10 +3,11 @@ class MovableObject {
     y = 250;
     height = 150;
     width = 80;
+    speed = 0.5;
     img;
-
     imageCache = [];
     imageChickenCache = [];
+    otherDirection = false;
 
     loadImage(path) {
         this.img = new Image();
@@ -29,9 +30,19 @@ class MovableObject {
         });
     }
 
-    moveRight() {
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+            if (this.x < -720) {
+                this.x = 600;
+            }
+        }, 1000 / 60);
     }
 
-    moveLeft() {
+    playAnimation(images) {
+        let i = this.currentImageIndex % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImageIndex++;
     }
 }
