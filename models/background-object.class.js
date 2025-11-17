84da9a8e1dc3af27c1e119526da863024f86f2a4
CANOST-id <csinner31@gmail.com>
2025-11-17 21:08:55 +0100

    added x param to constructor

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,9 +1,9 @@
 class BackgroundObject extends MovableObject {
 
-    constructor(imagePath) {
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.y = 0;
-        this.x = 0;
+        this.x = x;
         this.width = 720;
         this.height = 480;
     }
