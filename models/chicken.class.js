281b9f6e07374a3b58baa82a2b0fdbfa9022ddac
CANOST-id <csinner31@gmail.com>
2025-11-17 21:09:53 +0100

    edited sporn point and outsource function

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -5,29 +5,26 @@ class Chicken extends MovableObject {
         'assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
-    currentChickenImageIndex = 0;
+
+    currentImageIndex = 0;
+    speed = 0.25;
 
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
-        this.loadChickenImages(this.chicken_walk_images);
+        this.loadImages(this.chicken_walk_images);
 
         this.y = 340;
-        this.x = 200 + Math.random() * 500;
+        this.x = 400 + Math.random() * 1000;
         this.height = 80;
         this.width = 80;
         this.animateChicken();
+        this.speed = 0.25 + Math.random() * 0.6;
     }
 
     animateChicken() {
+        this.moveLeft();
         setInterval(() => {
-            this.x -= 0.8;
-            if (this.x < -80) {
-                this.x = 600;
-            }
-            let c = this.currentChickenImageIndex % this.chicken_walk_images.length;
-            let path = this.chicken_walk_images[c];
-            this.img = this.imageChickenCache[path];
-            this.currentChickenImageIndex++;
+            this.playAnimation(this.chicken_walk_images);
         }, 1000 / 10);
     }
 }
