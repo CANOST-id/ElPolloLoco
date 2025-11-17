96d461269da19e35da61bc81e57e34ea70ed22f2
CANOST-id <csinner31@gmail.com>
2025-11-17 21:13:23 +0100

    added eventListiner for keyboard

--- script/game.js
+++ script/game.js
@@ -1,9 +1,46 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
-    
+    world = new World(canvas, keyboard);
+
     console.log('My Character', world.character);
 }
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
+        keyboard.RIGHT = true;
+    }
+    if (e.key === 'ArrowLeft') {
+        keyboard.LEFT = true;
+    }
+    if (e.key === 'ArrowUp') {
+        keyboard.UP = true;
+    }
+    if (e.key === ' ') {
+        keyboard.SPACE = true;
+    }
+    if (e.key === 'd') {
+        keyboard.D = true;
+    }
+});
+window.addEventListener('keyup', (e) => {
+    if (e.key === 'ArrowRight') {
+        keyboard.RIGHT = false;
+    }
+    if (e.key === 'ArrowLeft') {
+        keyboard.LEFT = false;
+    }
+    if (e.key === 'ArrowUp') {
+        keyboard.UP = false;
+    }
+    if (e.key === ' ') {
+        keyboard.SPACE = false;
+    }
+    if (e.key === 'd') {
+        keyboard.D = false;
+    }
+});
