52e09e16d154528c614474ae59403d290303ca7f
CANOST-id <csinner31@gmail.com>
2025-11-17 21:10:31 +0100

    outsource function and changed position x

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,19 +1,16 @@
 class Cloud extends MovableObject {
 
+    speed = 0.15;
+
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/5_background/layers/4_clouds/1.png');
         this.y = 0;
-        this.x = Math.random() * 600 - 200;
+        this.x = Math.random() * 1800 - 200;
         this.animate();
         this.width = 720;
         this.height = 480;
     }
     animate() {
-        setInterval(() => {
-            this.x -= 0.25;
-            if (this.x < -720) {
-                this.x = 600;
-            }
-        }, 1000 / 60);
+        this.moveLeft();
     }
 }
