f69c6e38ae19d5d0b8490c1f647214c29cb20555
CANOST-id <csinner31@gmail.com>
2025-11-17 13:32:30 +0100

    animate clouds

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -3,8 +3,17 @@ class Cloud extends MovableObject {
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/5_background/layers/4_clouds/1.png');
         this.y = 0;
-        this.x = 10 + Math.random() * 600;
+        this.x = Math.random() * 600 - 200;
+        this.animate();
         this.width = 720;
         this.height = 480;
+    }
+    animate() {
+        setInterval(() => {
+            this.x -= 0.25;
+            if (this.x < -720) {
+                this.x = 600;
+            }
+        }, 1000 / 60);
     }
 }
