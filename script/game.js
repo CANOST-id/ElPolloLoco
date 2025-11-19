0b543692b2ea9fb8ca524520628f3d4983155021
CANOST-id <csinner31@gmail.com>
2025-11-19 21:46:15 +0100

    added startgame function and fullscreen functionality

--- script/game.js
+++ script/game.js
@@ -5,10 +5,7 @@ let keyboard = new Keyboard();
 function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
-
-    console.log('My Character', world.character);
 }
-
 
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight') {
@@ -44,3 +41,49 @@ window.addEventListener('keyup', (e) => {
         keyboard.D = false;
     }
 });
+
+function startScreen() {
+
+}
+
+function startGame() {
+    init();
+    disableStartGameButton();
+}
+
+function disableStartGameButton() {
+    let startButton = document.getElementById('start-button');
+    startButton.disabled = true;
+    startButton.style.backgroundColor = 'gray';
+}
+
+function enableStartGameButton() {
+    let startButton = document.getElementById('start-button');
+    startButton.disabled = false;
+    startButton.style.backgroundColor = '#f074155d';
+}
+
+function fullscreen() {
+    let elem = document.getElementById('fullscreen');
+    enterFullscreen(elem);
+}
+
+function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { 
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { 
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { 
+        element.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
