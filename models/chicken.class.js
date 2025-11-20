3d901c6f9f8d84abc7cf8059a1efd847b58474ca
CANOST-id <csinner31@gmail.com>
2025-11-20 15:12:11 +0100

    added dead chicken function

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,7 @@
 class Chicken extends MovableObject {
+    speed = 0.25;
+    energy = 20;
+    isDead = false;
 
     chicken_walk_images = [
         'assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
@@ -6,24 +9,42 @@ class Chicken extends MovableObject {
         'assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
-    speed = 0.25;
-
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
         this.loadImages(this.chicken_walk_images);
-
         this.y = 340;
         this.x = 600 + Math.random() * 1000;
         this.height = 80;
         this.width = 80;
-        this.animateChicken();
         this.speed = 0.25 + Math.random() * 0.6;
+        this.energy = 20;
+        this.animateChicken();
     }
 
     animateChicken() {
         this.moveLeft();
-        setInterval(() => {
-            this.playAnimation(this.chicken_walk_images);
+        this.walkInterval = setInterval(() => {
+            if (!this.isDead) {
+                this.playAnimation(this.chicken_walk_images);
+            }
         }, 1000 / 10);
+    }
+
+    hit(damage = 20) {
+        this.energy -= damage;
+        if (this.energy <= 0) {
+            this.energy = 0;
+            this.isDead = true;
+            this.chickenDeathAnimation();
+        }
+        console.log(`Chicken hit! Energy: ${this.energy}`);
+    }
+
+
+
+    isDead() {
+        
+        return this.energy <= 0;
+
     }
 }
