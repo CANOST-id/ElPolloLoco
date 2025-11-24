cfbe7decb99ec356052527c086b2b2e7f193a7d1
CANOST-id <csinner31@gmail.com>
2025-11-24 19:23:17 +0100

    fixed endscreen image and outsource style to style.css

--- models/endscreen.class.js
+++ models/endscreen.class.js
@@ -2,57 +2,51 @@ class Endscreen {
     canvas;
     ctx;
     isWin;
-    restartButton;
+    backToHomeButton;
+    endImage;
 
     constructor(canvas, isWin) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.isWin = isWin;
-        this.createRestartButton();
+        this.createEndscreen();
+        this.createButton();
     }
 
     createEndscreen() {
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        let endImage = new Image();
-        endImage.src = this.isWin ?
-            'assets/img_pollo_locco/img/9_intro_outro_screens/win/you win A.png' :
-            'assets/img_pollo_locco/img/9_intro_outro_screens/game_over/game over A.PNG';
-        endImage.onload = () => {
-            this.ctx.drawImage(endImage, 0, 0, this.canvas.width, this.canvas.height);
-        };
+        this.endImage = document.createElement('img');
+        this.endImage.src = this.isWin ?
+            'assets/img_pollo_locco/img/win_lost/you_win.png' :
+            'assets/img_pollo_locco/img/win_lost/you_lost.png';
+        this.endImageStyle();
+        document.body.appendChild(this.endImage);
     }
 
-    createRestartButton() {
-        this.restartButton = document.createElement('button');
-        this.designButton();
-        document.body.appendChild(this.restartButton);
-        this.buttonLisitener();
+    createButton() {
+        this.backToHomeButton = document.createElement('button');
+        this.backToHomeButtonStyle();
+        document.body.appendChild(this.backToHomeButton);
+        this.buttonListener();
     }
 
-    designButton() {
-        this.restartButton.innerHTML = 'RESTART GAME';
-        this.restartButton.style.position = 'absolute';
-        this.restartButton.style.left = '50%';
-        this.restartButton.style.top = '30%';
-        this.restartButton.style.transform = 'translateX(-50%)';
-        this.restartButton.style.padding = '15px 30px';
-        this.restartButton.style.fontSize = '20px';
-        this.restartButton.style.backgroundColor = '#ff6b35';
-        this.restartButton.style.color = 'white';
-        this.restartButton.style.border = 'none';
-        this.restartButton.style.borderRadius = '10px';
-        this.restartButton.style.cursor = 'pointer';
-        this.restartButton.style.zIndex = '1000';
+    endImageStyle() {
+        this.endImage.className = 'end-image';
     }
 
-    buttonLisitener() {
-        this.restartButton.addEventListener('click', () => {
+    backToHomeButtonStyle() {
+        this.backToHomeButton.innerHTML = 'Back to Home';
+        this.backToHomeButton.className = 'back-to-home-button';
+    }
+
+    buttonListener() {
+        this.backToHomeButton.addEventListener('click', () => {
             this.restartGame();
         });
     }
 
     restartGame() {
-        document.body.removeChild(this.restartButton);
+        document.body.removeChild(this.backToHomeButton);
+        document.body.removeChild(this.endImage);
         location.reload();
     }
 }
