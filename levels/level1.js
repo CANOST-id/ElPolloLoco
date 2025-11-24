45b723f1707954298978d9c021925ea46a2550b9
CANOST-id <csinner31@gmail.com>
2025-11-24 19:16:32 +0100

    format code

--- levels/level1.js
+++ levels/level1.js
@@ -1,16 +1,17 @@
-const level1 = new Level([
+const level1 = new Level(
+    [
         new Chicken(),
         new Chicken(),
         new Chicken(),
         new Chicken(),
         new Endboss()
     ],
-[
+    [
         new Cloud(),
         new Cloud(),
         new Cloud()
     ],
-[
+    [
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/air.png', 0),
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0),
