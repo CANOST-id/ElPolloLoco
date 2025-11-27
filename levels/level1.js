4055b8d7dbede695b71645e9df384dd24b6f56f7
CANOST-id <csinner31@gmail.com>
2025-11-27 15:10:36 +0100

    format code

--- levels/level1.js
+++ levels/level1.js
@@ -26,4 +26,5 @@ const level1 = new Level(
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 1440),
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 1440),
         new BackgroundObject('assets/img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 1440),
-    ]);
+    ]
+);
