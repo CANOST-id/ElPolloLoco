2dccca43f6af49c021d3281cfe87d9f60b23c034
CANOST-id <csinner31@gmail.com>
2025-11-28 16:22:47 +0100

    added walk and attack images

--- models/endboss-images.class.js
+++ models/endboss-images.class.js
@@ -1,5 +1,21 @@
 ENDBOSS_IMAGES = {
-    chicken_walk_images: [
+        chicken_walk_images: [
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G1.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G2.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G3.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G4.png'
+    ],
+    chicken_attack_images: [
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G13.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G14.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G15.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G16.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G17.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G18.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G19.png',
+        'assets/img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G20.png'
+    ],
+    chicken_alert_images: [
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G5.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G6.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -9,13 +25,11 @@ ENDBOSS_IMAGES = {
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G11.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G12.png'
     ],
-
     chicken_hurt_images: [
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G21.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G22.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G23.png'
     ],
-
     chicken_dead_images: [
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/5_dead/G24.png',
         'assets/img_pollo_locco/img/4_enemie_boss_chicken/5_dead/G25.png',
