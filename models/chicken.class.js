975dbb41fd039320ea9320086f25e40da86fdfb0
CANOST-id <csinner31@gmail.com>
2025-12-10 15:24:29 +0100

    added 2 more small chickens and redeem 1 normal chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,6 +8,12 @@ class Chicken extends MovableObject {
         'assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
+    small_chicken_walk_images = [
+        'assets/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'assets/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'assets/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+
     constructor() {
         super().loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
         this.loadImages(this.chicken_walk_images);
@@ -52,6 +58,40 @@ class Chicken extends MovableObject {
             bottom: 0,   
             left: 18,    
             right: 18    
+        };
+    }
+}
+
+class SmallChicken extends Chicken {
+    constructor() {
+        super();
+        this.loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/2_w.png');
+        this.loadImages(this.small_chicken_walk_images);
+        this.y = 365;
+        this.height = 55;
+        this.width = 55;
+    }
+
+    animateChicken() {
+        this.moveLeft();
+        this.walkInterval = setInterval(() => {
+            if (this.gameStarted && !this.isDead()) {
+                this.playAnimation(this.small_chicken_walk_images);
+            }
+        }, 1000 / 10);
+    }
+
+    chickenDeathAnimation() {
+        clearInterval(this.animateChicken);
+        this.loadImage('assets/img_pollo_locco/img/3_enemies_chicken/chicken_small/2_dead/dead.png');
+    }
+
+    getHitboxMargins() {
+        return { 
+            top: 8,
+            bottom: 0,   
+            left: 12,    
+            right: 12    
         };
     }
 }
